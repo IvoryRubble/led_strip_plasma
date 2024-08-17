ce4eaa5da3e60ca766fee1d5f9e0a524f2d65769
Yaroslav Metelev <yaroslav.metelev@outlook.com>
2024-08-17 08:44:39 +0700

    Implemented periodicFuncPow

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,92 @@
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
+
+// map() macro but for floats
+#define mapf(val, in_min, in_max, out_min, out_max) \
+  (float)(val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 #define LED_PIN 6
 #define LED_COUNT 29
 
-const uint32_t period = 50000;
-const int brightness0 = 100;
+const uint32_t period = 5000;
+const int brightness0 = 255;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
+float periodicFuncPow1(float);
+float periodicFuncPow2(float);
+float periodicFuncPow3(float);
+float periodicFuncPow4(float);
+
 void setup() {
   Serial.begin(115200);
-  strip.begin();          
-  strip.show();           
-  strip.setBrightness(100);
+  strip.begin();
+  strip.show();
+  strip.setBrightness(255);
 }
 
 void loop() {
   uint32_t currentTime = millis() % period;
-  
-  float pixelF = (float)(currentTime * LED_COUNT) / period;
-  float pixelT = pixelF - (int)pixelF;
-  float pixelF0 = (1 - pixelT) * (1 - pixelT) / 2;
-  float pixelF2 = (pixelT) * (pixelT) / 2;
-  float pixelF1 = 1 - pixelF0 - pixelF2;
-  int pixel0 = pixelF0 * brightness0;
-  int pixel1 = pixelF1 * brightness0;
-  int pixel2 = pixelF2 * brightness0;
+
+  float t = mapf(currentTime, 0, period, 0, 2);
+  float pixel1 = periodicFuncPow1(t);
+  float pixel2 = periodicFuncPow2(t);
+  float pixel3 = periodicFuncPow3(t);
+  float pixel4 = periodicFuncPow4(t);
+  int pixel1Int = pixel1 * brightness0;
+  int pixel2Int = pixel2 * brightness0;
+  int pixel3Int = pixel3 * brightness0;
+  int pixel4Int = pixel4 * brightness0;
 
   strip.clear();
-  strip.setPixelColor((int)pixelF + 0, strip.Color(pixel0, 0, 0, 0));
-  strip.setPixelColor((int)pixelF + 1, strip.Color(pixel1, 0, 0, 0));
-  strip.setPixelColor((int)pixelF + 2, strip.Color(pixel2, 0, 0, 0));
+  strip.setPixelColor(10, strip.Color(pixel1Int, 0, 0, 0));
+  strip.setPixelColor(14, strip.Color(pixel2Int, 0, 0, 0));
+  strip.setPixelColor(18, strip.Color(pixel3Int, 0, 0, 0));
+  strip.setPixelColor(22, strip.Color(pixel4Int, 0, 0, 0));
   strip.show();
-  delay(1); 
+  delay(1);
 }
 
+float periodicFuncPow1(float input) {
+  float x = input - ((int)input / 2 * 2);
+  float y;
+  if (x < 1) {
+    y = x;
+  } else {
+    y = 2 - x;
+  }
+  return y;
+}
+
+float periodicFuncPow2(float input) {
+  float x = input - ((int)input / 2 * 2);
+  float y;
+  if (x < 1) {
+    y = x * x;
+  } else {
+    y = (x - 2) * (x - 2);
+  }
+  return y;
+}
+
+float periodicFuncPow3(float input) {
+  float x = input - ((int)input / 2 * 2);
+  float y;
+  if (x < 1) {
+    y = x * x * x;
+  } else {
+    y = - (x - 2) * (x - 2) * (x - 2);
+  }
+  return y;
+}
+
+float periodicFuncPow4(float input) {
+  float x = input - ((int)input / 2 * 2);
+  float y;
+  if (x < 1) {
+    y = x * x * x * x;
+  } else {
+    y = (x - 2) * (x - 2) * (x - 2) * (x - 2);
+  }
+  return y;
+}
