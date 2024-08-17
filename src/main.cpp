a47ff775e30cad3eee198d298558401fadebb563
Yaroslav Metelev <yaroslav.metelev@outlook.com>
2024-08-17 10:30:11 +0700

    Implement running spots effect

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #define LED_COUNT 29
 
 const uint32_t period = 5000;
-const int brightness0 = 255;
+const int brightness0 = 254;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
@@ -18,6 +18,7 @@ float periodicFuncPow1(float);
 float periodicFuncPow2(float);
 float periodicFuncPow3(float);
 float periodicFuncPow4(float);
+float periodicFuncPow5(float);
 
 void setup() {
   Serial.begin(115200);
@@ -30,23 +31,13 @@ void loop() {
   uint32_t currentTime = millis() % period;
 
   float t = mapf(currentTime, 0, period, 0, 2);
-  float pixel0 = periodicFuncLinear(t, 0.75, 0.25);
-  float pixel1 = periodicFuncPow1(t);
-  float pixel2 = periodicFuncPow2(t);
-  float pixel3 = periodicFuncPow3(t);
-  float pixel4 = periodicFuncPow4(t);
-  int pixel0Int = pixel0 * brightness0;
-  int pixel1Int = pixel1 * brightness0;
-  int pixel2Int = pixel2 * brightness0;
-  int pixel3Int = pixel3 * brightness0;
-  int pixel4Int = pixel4 * brightness0;
 
   strip.clear();
-  strip.setPixelColor(6, strip.Color(pixel0Int, 0, 0, 0));
-  strip.setPixelColor(10, strip.Color(pixel1Int, 0, 0, 0));
-  strip.setPixelColor(14, strip.Color(pixel2Int, 0, 0, 0));
-  strip.setPixelColor(18, strip.Color(pixel3Int, 0, 0, 0));
-  strip.setPixelColor(22, strip.Color(pixel4Int, 0, 0, 0));
+  for (int i = 0; i < LED_COUNT; i++) {
+    float pixelI = periodicFuncPow5(t + 4 * (float)(i * 2) / LED_COUNT);
+    int pixelIInt = pixelI * brightness0 + 1; 
+    strip.setPixelColor(i, strip.Color(pixelIInt, 0, 0, 0));
+  }
   strip.show();
   delay(1);
 }
@@ -107,6 +98,17 @@ float periodicFuncPow4(float input) {
     y = x * x * x * x;
   } else {
     y = (x - 2) * (x - 2) * (x - 2) * (x - 2);
+  }
+  return y;
+}
+
+float periodicFuncPow5(float input) {
+  float x = input - ((int)input / 2 * 2);
+  float y;
+  if (x < 1) {
+    y = x * x * x * x * x;
+  } else {
+    y = - (x - 2) * (x - 2) * (x - 2) * (x - 2) * (x - 2);
   }
   return y;
 }
