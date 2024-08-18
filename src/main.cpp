82baa74afcd3d04258897f03179ce087a2090672
Yaroslav Metelev <yaroslav.metelev@outlook.com>
2024-08-18 19:04:18 +0700

    Add hue shift to plasma

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,13 @@
 #define LED_PIN 6
 #define LED_COUNT 29
 
-const uint32_t period0 = 3000;
+const uint32_t period0 = 5000;
 const uint32_t period1 = 7000;
+const uint32_t period2 = 10000;
+const uint32_t period3 = 3000;
 const int brightness0 = 255;
 const int brightness1 = 255;
+const int brightness2 = 255;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
@@ -22,6 +25,7 @@ float periodicFuncPow2(float);
 float periodicFuncPow3(float);
 float periodicFuncPow4(float);
 float periodicFuncPow5(float);
+float periodicFuncPow6(float);
 
 void setup() {
   Serial.begin(115200);
@@ -33,17 +37,25 @@ void setup() {
 void loop() {
   uint32_t currentTime0 = millis() % period0;
   uint32_t currentTime1 = millis() % period1;
+  uint32_t currentTime2 = millis() % period2;
+  uint32_t currentTime3 = millis() % period3;
 
-  float t0 = mapf(currentTime0, 0, period0, 100, 102);
-  float t1 = mapf(currentTime1, 0, period1, 100, 102);
+  float t0 = mapf(currentTime0, 0, period0, 1000, 1002);
+  float t1 = mapf(currentTime1, 0, period1, 1000, 1002);
+  float t2 = mapf(currentTime2, 0, period2, 1000, 1002);
+  float t3 = mapf(currentTime3, 0, period3, 1000, 1002);
 
   strip.clear();
   for (int i = 0; i < LED_COUNT; i++) {
-    float pixelI0 = periodicFuncPow5(t0 + 1.6 * (float)(i * 2) / LED_COUNT);
+    float pixelI0 = periodicFuncPow6(t0 + 1.6 * (float)(i * 2) / LED_COUNT);
     float pixelIInt0 = mapf(pixelI0, 0, 1, 0, brightness0); 
-    float pixelI1 = periodicFuncPow5(t1 - 4 * (float)(i * 2) / LED_COUNT);
-    float pixelIInt1 = mapf(pixelI1, 0, 1, 0, brightness1); 
-    strip.setPixelColor(i, strip.ColorHSV(65535 / 2, 255, (int)((pixelIInt0 + pixelIInt1) / 2)));
+    float pixelI1 = periodicFuncPow6(t1 - 4 * (float)(i * 2) / LED_COUNT);
+    float pixelIInt1 = mapf(pixelI1, 0, 1, 0, brightness1);
+    float pixelI2 = periodicFuncPow1(t2) * 4 / 6 + periodicFuncPow1(t3 + 3 * (float)(i * 2) / LED_COUNT) * 2 / 6;
+    float pixelIInt2 = mapf(pixelI2, 0, 1, 0, UINT16_MAX);
+    int val = (int)((pixelIInt0 + pixelIInt1) / 2);
+    int hue = (int)pixelIInt2;
+    strip.setPixelColor(i, strip.ColorHSV(hue, 255, val));
   }
   strip.show();
   delay(10);
@@ -122,6 +134,17 @@ float periodicFuncPow5(float input) {
     y = x * x * x * x * x;
   } else {
     y = - (x - 2) * (x - 2) * (x - 2) * (x - 2) * (x - 2);
+  }
+  return y;
+}
+
+float periodicFuncPow6(float input) {
+  float x = input - ((int)input / 2 * 2);
+  float y;
+  if (x < 1) {
+    y = x * x * x * x * x * x;
+  } else {
+    y = (x - 2) * (x - 2) * (x - 2) * (x - 2) * (x - 2) * (x - 2);
   }
   return y;
 }
